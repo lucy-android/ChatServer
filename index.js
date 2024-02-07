149220906b45456c8f19ae0668ac5e5d1ad3b7e5
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2024-02-07 13:45:50 +0300

    ChS: add code to connect to websocket

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 const express = require('express'),
-      http = require('http'),
-      app = express(),
-      server = http.createServer(app),
-      { Server } = require('socket.io'),
-      io = new Server(server);
+  http = require('http'),
+  app = express(),
+  server = http.createServer(app),
+  { Server } = require('socket.io'),
+  io = new Server(server);
 
 app.get('/', (req, res) => {
   res.send('Chat Server is running on port 3000');
+});
+
+io.on('connection', (socket) => {
+
+  console.log('user connected')
+
+  socket.on('join', function (userNickname) {
+
+    console.log(userNickname + " : has joined the chat ")
+
+    socket.broadcast.emit('userjoinedthechat', userNickname + " : has joined the chat ")
+
+    socket.on('messagedetection', (senderNickname, messageContent) => {
+
+      //log the message in console 
+
+      console.log(senderNickname + " :" + messageContent)
+      //create a message object
+
+      let message = { "message": messageContent, "senderNickname": senderNickname }
+
+      // send the message to the client side  
+
+      socket.emit('message', message)
+
+    });
+
+
+    socket.on('disconnect', function () {
+      console.log('user has left ')
+      socket.broadcast.emit("userdisconnect", ' user has left')
+    });
+
+  });
+
 });
 
 server.listen(3000, () => {
