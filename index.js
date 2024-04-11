1d81f553e0a9b7da25305f548c7e0a6f9affd53b
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2024-04-11 23:30:50 +0300

    ChS: parse json

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const express = require('express')
 const webserver = express()
- .listen(3000, () => console.log(`Listening on ${3000}`))
-
- let myList = []
+  .listen(3000, () => console.log(`Listening on ${3000}`))
 
 const { WebSocketServer } = require('ws')
 const socketserver = new WebSocketServer({ port: 443 })
 socketserver.on('connection', ws => {
- console.log('New client connected!')
- ws.on('close', () => console.log('Client has disconnected!'))
- ws.on('message', data => {
-   socketserver.clients.forEach(client => {
-    if(data.toString().endsWith("has connected to the chat")){
-      const list = data.toString().split(" ")
-      myList.push(list[0])
-    }
-     client.send(`${myList}`)
-   })
- })
- ws.onerror = function () {
-   console.log('websocket error')
- }
+  console.log('New client connected!')
+  ws.on('close', () => console.log('Client has disconnected!'))
+  ws.on('message', data => {
+    socketserver.clients.forEach(client => {
+
+      let decoder = new TextDecoder("utf-8");
+      let str = decoder.decode(data);
+      console.log("JSON.parse(str).isMessage: ", JSON.parse(str).isMessage)
+      
+
+      if (JSON.parse(str).isMessage == false) {
+        myList.push(JSON.parse(str).contents)
+      }
+      client.send(JSON.parse(str).contents)
+    })
+  })
+  ws.onerror = function () {
+    console.log('websocket error')
+  }
 })
