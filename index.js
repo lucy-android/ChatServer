6b689a08029a118e5b6bc7e74bd373f47b0ed269
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2024-04-26 23:24:37 +0300

    ChS: implement sending message to clients

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 const express = require('express')
 const webserver = express()
- .listen(3000, () => console.log(`Listening on ${3000}`))
+  .listen(3000, () => console.log(`Listening on ${3000}`))
 
- let myList = []
+let userList = []
 
 const { WebSocketServer } = require('ws')
 const socketserver = new WebSocketServer({ port: 443 })
-socketserver.on('connection', ws => {
- console.log('New client connected!')
- ws.on('close', () => console.log('Client has disconnected!'))
- ws.on('message', data => {
-   socketserver.clients.forEach(client => {
-    if(data.toString().startsWith("/")){
-      const list = data.toString().split("/")
-      myList.push(list[1])
-    }
-     client.send(`${myList}`)
-   })
- })
- ws.onerror = function () {
-   console.log('websocket error')
- }
+socketserver.on('connection', ws => { 
+  console.log('New client connected!')
+  ws.on('close', () => console.log('Client has disconnected!'))
+  ws.on('message', data => {
+    socketserver.clients.forEach(client => {
+      const jsonString = new TextDecoder().decode(new Uint8Array(data))
+      const jsonData = JSON.parse(jsonString)
+      console.log("jsonString: ", jsonString)
+      console.log("jsonString isGreeting: ", jsonData.isGreeting)
+      if (jsonData.isGreeting == true) {
+        userList.push(jsonData.contents)
+        const userId = userList.length
+        client.send(JSON.stringify({
+          id: userId,
+          isGreeting: true,
+          contents: jsonData.contents
+        })
+        )
+      }
+    })
+  })
+  ws.onerror = function () {
+    console.log('websocket error')
+  }
 })
